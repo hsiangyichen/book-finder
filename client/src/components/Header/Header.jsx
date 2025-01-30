2a512ab60e9659126b5fa3dce2d89188e8c23c71
Chandni Melwani <cmelwani92@gmail.com>
2025-01-30 17:02:01 -0500

    added styling to header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,21 +1,38 @@
+// import { Link } from "react-router-dom";
+// import "./Header.scss";
+
+// const Header = () => {
+//   return (
+//     <header className="header section-container">
+//       <h1 className="header__title">BookHunt</h1>
+//       <nav className="header__nav">
+//         <ul className="header__nav-list">
+//           <li className="header__nav-item">
+//             <Link to="/" className="header__nav-link">
+//               Home
+//             </Link>
+//           </li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+
 import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
-  return (
-    <header className="header section-container">
-      <h1 className="header__title">BookHunt</h1>
-      <nav className="header__nav">
-        <ul className="header__nav-list">
-          <li className="header__nav-item">
-            <Link to="/" className="header__nav-link">
-              Home
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className="header">
+            <h1 className="header__title">📚 BookHunt</h1>
+            <nav className="header__nav">
+                <Link to="/">Home</Link>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
