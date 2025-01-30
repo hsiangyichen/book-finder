0cdbbbf929878344d9ac192f6b22bca0708810c9
hsiangyichen <hsiangyi1025@gmail.com>
2025-01-30 10:50:09 -0800

    Add frontend design

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer section-container">
+      <p className="footer__text">
+        © 2024 Book Finder | Built with ❤️ by 404 Book Not Found
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
